509982b73bb09b452f36125589d201a7299f2ff0
Solo <shineboboaung00@gmail.com>
2026-07-15 04:58:52 +0630

    Python (File Handling)

--- project/do_to_list(Def).py
+++ project/do_to_list(Def).py
@@ -1,4 +1,20 @@
 do_to = []
 
+print ("1.show 2.add 3.remove 4.exit")
+
+user = input("Choice the number: ")
+
+if user == "1":
+    show()
+elif user == "2":
+    add()
+elif user == "3":
+    remove()
+elif user == "4":
+    print("Thanks You For Us")
+    break
+
+
 def show():
-    return(f"")
+    return(do_to)
+
