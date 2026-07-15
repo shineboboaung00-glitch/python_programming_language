a4d959e3ebb577211fb58eaf3a904fca4b700133
Solo <shineboboaung00@gmail.com>
2026-07-16 05:12:08 +0630

    try...except,data_structure_(16.7.26)

--- project/do_to_list(Def).py
+++ project/do_to_list(Def).py
@@ -1,20 +1,32 @@
 do_to = []
 
-print ("1.show 2.add 3.remove 4.exit")
-
-user = input("Choice the number: ")
-
-if user == "1":
-    show()
-elif user == "2":
-    add()
-elif user == "3":
-    remove()
-elif user == "4":
-    print("Thanks You For Us")
-    break
 
 
 def show():
     return(do_to)
 
+def add():
+    user_input = input("Enter the Task: ")
+    do_to.append(user_input)
+    print(f"{user_input} is add Sucressful")
+
+
+def remove():
+    user_input = input("Enter the remove Task: ")
+    do_to.remove(user_input)
+    print(f"{user_input} is remove Sucressful")
+
+print ("1.show 2.add 3.remove 4.exit")
+
+
+while True:
+        user = input("Choice the number: ")
+        if user == "1":
+            show()
+        elif user == "2":
+            add()
+        elif user == "3":
+            remove()
+        elif user == "4":
+            print("Thanks You For Us")
+            break
